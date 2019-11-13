4650293fa79220c31d9bced87a00edecfca8151a
Wizerdy <grancru2000@gmail.com>
2019-11-13 16:29:28 +0100

    Basics
    Hierarchy :
    - Character
    
    Scripts :
    - CharacterController

---
+++ Leaps and bounds/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float _speed;
+
+    private Rigidbody2D _rgBd;
+
+    void Start()
+    {
+        _rgBd = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            _rgBd.velocity = new Vector2(
+                Input.GetAxis("Horizontal") * _speed * Time.deltaTime, 
+                Input.GetAxis("Vertical") * _speed * Time.deltaTime);
+    }
+}
