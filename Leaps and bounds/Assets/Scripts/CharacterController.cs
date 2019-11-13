6fdf60b81df4ca3bebbd2d47d50780744483fb98
Wizerdy <grancru2000@gmail.com>
2019-11-13 17:23:43 +0100

    Character Controller
    Dash + (Roll)

--- Leaps and bounds/Assets/Scripts/CharacterController.cs
+++ Leaps and bounds/Assets/Scripts/CharacterController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public float _speed;
+    public Vector2 _speed;
+    public float _dashSpeed;
+    public float _rollSpeed;
+    public float _dashTime;
 
     private Rigidbody2D _rgBd;
+    private bool _dashing;
 
     void Start()
     {
         _rgBd = GetComponent<Rigidbody2D>();
+        _dashing = false;
     }
 
     void Update()
@@ -21,8 +26,28 @@ public class CharacterController : MonoBehaviour
     private void Move()
     {
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-            _rgBd.velocity = new Vector2(
-                Input.GetAxis("Horizontal") * _speed * Time.deltaTime, 
-                Input.GetAxis("Vertical") * _speed * Time.deltaTime);
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !_dashing && (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0))
+                StartCoroutine(Dash(_dashTime, new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")), _dashSpeed));
+            else if (Input.GetKeyDown(KeyCode.Space) && !_dashing && (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0))
+                StartCoroutine(Dash(_dashTime, new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")), _rollSpeed));
+            else if (!_dashing)
+                _rgBd.velocity = new Vector2(
+                    Input.GetAxisRaw("Horizontal") * _speed.x * Time.deltaTime,
+                    Input.GetAxisRaw("Vertical") * _speed.y * Time.deltaTime);
+        }
+
+    }
+
+    IEnumerator Dash(float time, Vector2 direction, float speed)
+    {
+        _dashing = true;
+        for (int i = 0; i < 10; i++)
+        {
+            _rgBd.velocity = new Vector2(speed * Time.deltaTime * direction.x, speed * Time.deltaTime * direction.y);
+            yield return new WaitForSeconds(time / 10);
+        }
+        _rgBd.velocity = new Vector2(0, 0);
+        _dashing = false;
     }
 }
